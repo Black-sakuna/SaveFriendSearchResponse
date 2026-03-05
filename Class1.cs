858f2ddbea21b8c4841e349fb9eb68af10f551b1
Black-sakuna <547922037@qq.com>
2026-03-06 01:41:55 +0800

    新建文件夹

--- Class1.cs
+++ Class1.cs
@@ -1,7 +1,119 @@
-﻿namespace SaveFriendSearchResponse
+﻿using Gallop;
+using Newtonsoft.Json.Linq;
+using Spectre.Console;
+using UmamusumeResponseAnalyzer;
+using UmamusumeResponseAnalyzer.Plugin;
+
+namespace SaveFriendSearchResponse
 {
-    public class Class1
+    public class SaveFriendSearchResponse : IPlugin
     {
 
+        [PluginDescription("保存好友种马信息")]
+        public string Name => "SaveFriendSearchResponse";
+
+        public Version Version => new Version(1, 0, 0);
+
+        public string Author => "匿名";
+
+        public string[] Targets => [];
+
+        internal string PLUGIN_DATA_DIRECTORY = string.Empty;
+
+        internal string charaViewerId = string.Empty;
+
+        internal string charaUmaName = string.Empty;
+
+        internal string joData = string.Empty;
+
+        private CancellationTokenSource cts = new CancellationTokenSource();
+
+        private Task task1;
+
+        public Task UpdatePlugin(ProgressContext ctx)
+        {
+            return Task.CompletedTask;
+        }
+        public void Initialize()
+        {
+            if(UmamusumeResponseAnalyzer.UmamusumeResponseAnalyzer.Started)
+            {
+                PLUGIN_DATA_DIRECTORY = Path.Combine("PluginData", Name);
+                task1 = Task.Run(ConsoleReadKeyCheck);
+            }
+        }
+        public void Dispose()
+        {
+            cts.Cancel();
+        }
+        [Analyzer(true, 0)]
+        public void Analyze(JObject jo)
+        {
+            if (jo.ContainsKey("data") && jo["data"] is JObject data && data.ContainsKey("user_info_summary"))
+            {
+                bool b1 = data.ContainsKey("partner_chara_info_array");
+                bool b2 = data.ContainsKey("support_card_data_array");
+                bool b3 = data.ContainsKey("follower_num");
+                bool b4 = data.ContainsKey("own_follow_num");
+                if (b1 && b2 && b3 && b4)
+                {
+                    string charaViewerId = string.Empty;
+                    string charaUmaName = string.Empty;
+                    string joData = string.Empty;
+                    FriendSearchResponse @event = jo.ToObject<FriendSearchResponse>();
+                }
+            }
+        }
+        private void ConsoleReadKeyCheck()
+        {
+            CancellationToken token = cts.Token;
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    string keyChar = Console.ReadKey(true).KeyChar.ToString();
+                    if (keyChar == "x" || keyChar == "X")
+                    {
+                        if (File.Exists(PLUGIN_DATA_DIRECTORY + "\\tmp.txt"))
+                        {
+                            JObject jo = JObject.Parse(File.ReadAllText(PLUGIN_DATA_DIRECTORY + "\\tmp.txt"));
+                        }
+                    }
+                    else if (keyChar == "c" || keyChar == "C")
+                    {
+                        ShowFriendSearch();
+                    }
+                }
+            }
+            catch (OperationCanceledException ex)
+            {
+                Console.WriteLine("ConsoleReadKeyCheck：" + ex.Message);
+            }
+        }
+
+        private void ShowFriendSearch()
+        {
+            string selection = string.Empty;
+            do
+            {
+                List<string> choices = new List<string>
+            {
+                "插件_ " + Name,
+                $"版本_ {Version}",
+                "作者_ " + Author
+            };
+                choices.Add("返回");
+                SelectionPrompt<string> selectionPrompt = new SelectionPrompt<string>().Title("种马信息：").WrapAround().AddChoices(choices);
+                selection = AnsiConsole.Prompt(selectionPrompt);
+                if (selection != "返回")
+                {
+                    JObject jo = JObject.Parse(File.ReadAllText(Path.Combine(PLUGIN_DATA_DIRECTORY, "tmp.txt")));
+                    ParseFriendSearch.ParseFriendSearchResponse(jo.ToObject<FriendSearchResponse>());
+                    Console.WriteLine("按任意键返回。");
+                    Console.ReadKey();
+                }
+            }
+            while (selection != "返回");
+        }
     }
 }
---
+++ ParseFriendSearch.cs
@@ -0,0 +1,93 @@
+﻿using Gallop;
+using Spectre.Console;
+using System.Text;
+using UmamusumeResponseAnalyzer;
+
+namespace SaveFriendSearchResponse
+{
+    public class ParseFriendSearch
+    {
+        private static readonly string I18N_Friend = "好友：{0}\t\tID：{1}\t\tFollower数：{2}";
+
+        private static readonly string I18N_Uma = "种马：{0}\t胜鞍：{1}\t\t评分：{2}";
+
+        private static readonly string I18N_WinSaddle = "胜鞍列表：{0}";
+
+        private static readonly string I18N_Factor = "因子";
+
+        private static readonly string I18N_UmaFactor = "代表";
+
+        private static readonly string I18N_ParentFactor = "祖辈@{0}";
+
+        public static void ParseFriendSearchResponse(FriendSearchResponse @event)
+        {
+            FriendSearchResponse.CommonResponse data = @event.data;
+            TrainedChara chara = data.partner_chara_info_array[0];
+            IEnumerable<int> charaWinSaddle = chara.win_saddle_id_array.Intersect(Database.SaddleIds);
+            IEnumerable<int> parentWinSaddle_a = chara.succession_chara_array[0].win_saddle_id_array.Intersect(Database.SaddleIds);
+            IEnumerable<int> parentWinSaddle_b = chara.succession_chara_array[1].win_saddle_id_array.Intersect(Database.SaddleIds);
+            int friendAndDadWinSaddle = charaWinSaddle.Intersect(parentWinSaddle_a).Count() * 3;
+            int friendAndMomWinSaddle = charaWinSaddle.Intersect(parentWinSaddle_b).Count() * 3;
+            AnsiConsole.Write(new Rule());
+            AnsiConsole.WriteLine(I18N_Friend, data.user_info_summary.name, data.user_info_summary.viewer_id, data.follower_num);
+            AnsiConsole.WriteLine(I18N_Uma, Database.Names.GetUmamusume(chara.card_id).FullName, friendAndDadWinSaddle + friendAndMomWinSaddle, chara.rank_score);
+            AnsiConsole.WriteLine(I18N_WinSaddle, string.Join(',', charaWinSaddle));
+            Tree tree = new Tree(I18N_Factor);
+            int max = chara.factor_info_array.Select((FactorInfo x) => x.factor_id).Concat(chara.succession_chara_array[0].factor_info_array.Select((FactorInfo x) => x.factor_id)).Concat(chara.succession_chara_array[1].factor_info_array.Select((FactorInfo x) => x.factor_id))
+                .Where((int x, int index) => index % 2 == 0)
+                .Max((int x) => GetRenderWidth(Database.FactorIds[x]));
+            Tree representative = AddFactors(I18N_UmaFactor, chara.factor_info_array.Select((FactorInfo x) => x.factor_id).ToArray(), max);
+            Tree inheritanceA = AddFactors(string.Format(I18N_ParentFactor, chara.succession_chara_array[0].owner_viewer_id), chara.succession_chara_array[0].factor_info_array.Select((FactorInfo x) => x.factor_id).ToArray(), max);
+            Tree inheritanceB = AddFactors(string.Format(I18N_ParentFactor, chara.succession_chara_array[1].owner_viewer_id), chara.succession_chara_array[1].factor_info_array.Select((FactorInfo x) => x.factor_id).ToArray(), max);
+            tree.AddNodes(representative, inheritanceA, inheritanceB);
+            AnsiConsole.Write(tree);
+            AnsiConsole.Write(new Rule());
+        }
+
+        public static int GetRenderWidth(string text)
+        {
+            return text.Sum((char x) => x.GetCellWidth());
+        }
+
+        public static Tree AddFactors(string title, int[] id_array, int max)
+        {
+            Tree tree = new Tree(title);
+            IEnumerable<int> ordered = id_array.Take(2).Append(id_array[^1]).Concat(id_array.Skip(2).SkipLast(1));
+            int[] even = ordered.Where((int x, int index) => index % 2 == 0).ToArray();
+            int[] odd = ordered.Where((int x, int index) => index % 2 != 0).ToArray();
+            foreach (int index2 in Enumerable.Range(0, even.Length))
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append(FactorName(even[index2]));
+                int gap = 12 + max - GetRenderWidth(Database.FactorIds[even[index2]]);
+                if (gap < 0)
+                {
+                    gap = 2;
+                }
+                sb.Append(string.Join(string.Empty, Enumerable.Repeat(' ', gap)));
+                sb.Append((odd.Length > index2) ? FactorName(odd[index2]) : "");
+                tree.AddNode(sb.ToString());
+            }
+            return tree;
+        }
+        public static string FactorName(int factorId)
+        {
+            string name = Database.FactorIds[factorId];
+            int length = factorId.ToString().Length;
+            if (1 == 0)
+            {
+            }
+            string result = length switch
+            {
+                3 => "[#FFFFFF on #37B8F4]" + name + " [/]",
+                4 => "[#FFFFFF on #FF78B2]" + name + " [/]",
+                8 => "[#794016 on #91D02E]" + name + " [/]",
+                _ => "[#794016 on #E1E2E1]" + name + " [/]",
+            };
+            if (1 == 0)
+            {
+            }
+            return result;
+        }
+    }
+}
